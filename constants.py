4851188d7ab92a2d158c0860f0ed6487daa80eb0
txoof <aaron.ciuffo@gmail.com>
2020-06-13 12:59:23 +0200

    first commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+appName = 'portfolioCreator'
+develName = 'com.txoof'
+userPath = '.'.join((develName, appName))
+configFile = 'portfolioCreator.ini'
+
+version = '0.0.1'
+contact = 'aaron.ciuffo@gmail.com'
