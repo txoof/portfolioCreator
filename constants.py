d7dc237747656b8fc2dacdcf454e3b44bcf7a507
txoof <aaron.ciuffo@gmail.com>
2020-06-25 08:53:36 +0200

    work

--- constants.py
+++ constants.py
@@ -1,7 +1,37 @@
-appName = 'portfolioCreator'
-develName = 'com.txoof'
-userPath = '.'.join((develName, appName))
-configFile = 'portfolioCreator.ini'
-
+from pathlib import Path
 version = '0.0.1'
+app_name = 'createFolders'
+devel_name = 'com.txoof'
 contact = 'aaron.ciuffo@gmail.com'
+git_repo = f'github.com/txoof/{app_name}'
+
+config_file = '.'.join((app_name, 'ini'))
+config_dir = '.'.join((devel_name, app_name))
+user_config_path = Path('~').expanduser()/config_dir/config_file
+
+# sentry file that program expects to find in root of Cumulative folders dir
+sentry_file = 'sentryFile_DO_NOT_REMOVE.txt'
+
+# headers and data types expected in CSV file export from PS
+expected_headers ={'LastFirst': str, 'ClassOf': int, 'Student_Number': int}
+
+# number of times to attempt to confirm remote file creation
+confirm_retry = 4
+# multiplier for each retry cycle - confirm_retry * base_wait
+base_wait = 10
+
+# student grade level directories to create:
+student_dirs = ['00-Preschool', 
+                '00-Transition Kindergarten',
+                '01-Grade',
+                '02-Grade',
+                '03-Grade',
+                '04-Grade',
+                '05-Grade',
+                '06-Grade',
+                '07-Grade',
+                '08-Grade',
+                '09-Grade',
+                '10-Grade',
+                '11-Grade',
+                '12-Grade']
