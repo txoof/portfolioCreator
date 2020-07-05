e4ead24f53d1550c3c949aae6a74e798f69c4d01
txoof <aaron.ciuffo@gmail.com>
2020-07-05 12:13:42 +0200

    restructured project

--- class_constants.py
+++
@@ -1,7 +0,0 @@
-# constants for classes #
-#########################
-
-# webview base path for files
-file_base = 'https://drive.google.com/file/d/'
-# webview base path for folders/directories
-dir_base = 'https://drive.google.com/drive/folders/'
