e6e624b0efdd44584e1d10fbd2f254fa32f2fbbf
txoof <aaron.ciuffo@gmail.com>
2020-07-29 12:42:07 +0200

    update build scripts

--- publish.sh
+++ publish.sh
@@ -1,12 +1,21 @@
 #!/bin/zsh
 source_path=./create_folders
+publish_tar=portfolioCreator.tgz
+
+if [ -z "$1" ]; then
+  echo Build a pyinstaller, tar build, tag and push release to github
+  echo usage:
+  echo $0 "release comment"
+  exit 0
+fi
 
 # run the build script
 ./build.sh
 
 version_number=`grep version $source_path/constants.py | sed -nE  's/^version[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
 
-git tag $version_number
-git commit -m "update tar distribution"
+tag = "v$version_number"
 
-git push
+git tag $tag
+git commit -m "update tar distribution $1" $publish_tar
+git push origin $tag $1
