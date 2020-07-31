a683959aef0d14628cc12e81daee6936b5f4d236
txoof <aaron.ciuffo@gmail.com>
2020-07-31 16:18:22 +0200

    fix push

--- publish.sh
+++ publish.sh
@@ -1,21 +1,24 @@
 #!/bin/zsh
 source_path=./create_folders
+
 publish_tar=createFolders.tgz
 
+version_number=`grep version $source_path/constants.py | sed -nE  's/^version[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
+
+
 if [ -z "$1" ]; then
+  echo current version number: v$version_number
   echo tar build, tag and push release to github
   echo usage:
-  echo $0 "release comment"
+  echo $0 \"release comment\"
   exit 0
 fi
 
 # run the build script
 #./build.sh
 
-version_number=`grep version $source_path/constants.py | sed -nE  's/^version[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
-
 tag="v$version_number"
 
 git tag -a "$tag" -m "$1"
 git commit -m "update tar distribution $1" $publish_tar
-git push origin $tag $1
+git push origin $tag 
