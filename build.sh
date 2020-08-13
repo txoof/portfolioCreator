ccd4446994a6218d100f39183b27f4562e6ec520
txoof <aaron.ciuffo@gmail.com>
2020-08-13 22:12:16 +0200

    add --clean

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 app_name='createFolders'
 
 pushd ./create_folders
-pipenv run pyinstaller $app_name.spec --noconfirm
+pipenv run pyinstaller $app_name.spec --noconfir --clean
 pushd ./dist
 tar cvzf  ../../$app_name.tgz ./$app_name
 popd
