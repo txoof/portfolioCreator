35bae7017e899a9ca541d145e8b93f45a2bba2c3
txoof <aaron.ciuffo@gmail.com>
2020-09-06 11:27:52 +0200

    automate conversion of ipynb

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 app_name='createFolders'
 
 pushd ./create_folders
+~/bin/develtools/nbconvert createFolders.ipynb
 pipenv run pyinstaller $app_name.spec --noconfir --clean
 pushd ./dist
 tar cvzf  ../../$app_name.tgz ./$app_name
