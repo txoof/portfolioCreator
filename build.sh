fffedbd09b578ef1882cdc9a0978a4c08b449356
txoof <aaron.ciuffo@gmail.com>
2020-09-07 11:25:48 +0200

    build script regenerates spec file on each build -- prevents bad paths from creeping in when building on different machines

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ app_name='createFolders'
 
 pushd ./create_folders
 ~/bin/develtools/nbconvert createFolders.ipynb
-pipenv run pyinstaller $app_name.spec --noconfir --clean
+pipenv run pyinstaller --onefile --noconfirm --clean --add-data Help.md:. --add-data logging_cfg.ini:. --add-data createFolders.ini:. --exclude-module IPython $app_name.py
+#pipenv run pyinstaller $app_name.spec --noconfir --clean
 pushd ./dist
 tar cvzf  ../../$app_name.tgz ./$app_name
 popd
