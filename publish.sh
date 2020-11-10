47b72d8e68cb9fd764b4c52f66c71951d0ead74a
txoof <aaron.ciuffo@gmail.com>
2020-11-10 09:23:44 +0100

    switch to codesigning, packaging

--- publish.sh
+++ publish.sh
@@ -4,18 +4,21 @@ source_path=./create_folders
 #publish_tar=createFolders.tgz
 publish_pkg=createfolders.pkg
 
+
 version_number=`grep version $source_path/constants.py | sed -nE  's/^version[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
 
 
 if [ -z "$1" ]; then
   echo current version number: v$version_number
-  echo tar build, tag and push release to github
+  echo pkg build, sign notarize, tag and push release to github
   echo usage:
   echo $0 \"release comment\"
   exit 0
 fi
 
 tag="v$version_number"
+
+pycodesign.py codesign.ini
 
 git tag -a "$tag" -m "$1"
 git commit -m "update pkg distribution $1" $publish_pkg
